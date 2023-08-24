9f5de245de8c1090f6e2978991ffde7f31a22a3b
ideyedi <gdregas18@gmail.com>
2023-08-24 15:06:45 +0900

    에러 관련 누락내역 추가

---
+++ app/error.py
@@ -0,0 +1,29 @@
+from marshmallow import Schema, fields
+
+
+class ApiErrorSchema(Schema):
+    status_code = fields.Integer(data_key="code", required=True)
+    error_messages = fields.String
+
+
+class ApiError(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+
+        if status_code is not None:
+            self.status_code = status_code
+
+        self.payload = payload
+
+    def to_dict(self, debug=True):
+        rv = dict(message=self.message)
+        if debug and self.payload:
+            rv.update(self.payload)
+
+        if debug and self.__cause__:
+            rv["cause"] = str(self.__cause__)
+
+        return rv
